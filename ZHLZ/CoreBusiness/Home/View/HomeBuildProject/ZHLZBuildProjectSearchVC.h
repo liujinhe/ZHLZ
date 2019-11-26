a8e90425fbdc2d41fc936c3dd3604b1df82f796d
lijinhui <854816128@qq.com>
2019-11-26 18:02:27 +0800

    李锦辉提交==

---
+++ ZHLZ/CoreBusiness/Home/View/HomeBuildProject/ZHLZBuildProjectSearchVC.h
@@ -0,0 +1,17 @@
+//
+//  ZHLZBuildProjectSearchVC.h
+//  ZHLZ
+//
+//  Created by apple on 2019/11/26.
+//  Copyright © 2019 liujinhe. All rights reserved.
+//
+
+#import "ZHLZBaseVC.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZHLZBuildProjectSearchVC : ZHLZBaseVC
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZHLZ/CoreBusiness/Home/View/HomeBuildProject/ZHLZBuildProjectSearchVC.m
@@ -0,0 +1,32 @@
+//
+//  ZHLZBuildProjectSearchVC.m
+//  ZHLZ
+//
+//  Created by apple on 2019/11/26.
+//  Copyright © 2019 liujinhe. All rights reserved.
+//
+
+#import "ZHLZBuildProjectSearchVC.h"
+
+@interface ZHLZBuildProjectSearchVC ()
+
+@end
+
+@implementation ZHLZBuildProjectSearchVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
