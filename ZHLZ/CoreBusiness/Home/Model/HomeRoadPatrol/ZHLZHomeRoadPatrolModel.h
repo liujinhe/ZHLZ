8a75c40615dd216243766b69b2c0b64c0247d6c4
liujinhe <liujinhe1060@rongjinsuo.com>
2019-11-27 11:07:24 +0800

    提交

--- ZHLZ/CoreBusiness/Home/Model/HomeRoadPatrol/ZHLZHomeRoadPatrolModel.h
+++ ZHLZ/CoreBusiness/Home/Model/HomeRoadPatrol/ZHLZHomeRoadPatrolModel.h
@@ -12,6 +12,55 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface ZHLZHomeRoadPatrolModel : ZHLZBaseModel
 
+@property (nonatomic, copy) NSString *areaid;
+@property (nonatomic, copy) NSString *areaname;
+@property (nonatomic, copy) NSString *checkingnum;
+@property (nonatomic, copy) NSString *clooplv;
+@property (nonatomic, copy) NSString *closenum;
+@property (nonatomic, copy) NSString *closing;
+@property (nonatomic, copy) NSString *createdate;
+@property (nonatomic, copy) NSString *entrydate;
+@property (nonatomic, copy) NSString *filing;
+@property (nonatomic, copy) NSString *findnum;
+@property (nonatomic, copy) NSString *fine;
+@property (nonatomic, copy) NSString *foundnum;
+@property (nonatomic, copy) NSString *isWork;
+@property (nonatomic, copy) NSString *mapMileage;
+@property (nonatomic, copy) NSString *mi10num;
+@property (nonatomic, copy) NSString *mi11num;
+@property (nonatomic, copy) NSString *mi12num;
+@property (nonatomic, copy) NSString *mi14num;
+@property (nonatomic, copy) NSString *mi15num;
+@property (nonatomic, copy) NSString *mi16num;
+@property (nonatomic, copy) NSString *mi17num;
+@property (nonatomic, copy) NSString *mi18num;
+@property (nonatomic, copy) NSString *mi19num;
+@property (nonatomic, copy) NSString *mi20;
+@property (nonatomic, copy) NSString *mi201num;
+@property (nonatomic, copy) NSString *mi202num;
+@property (nonatomic, copy) NSString *mi203num;
+@property (nonatomic, copy) NSString *mi204num;
+@property (nonatomic, copy) NSString *mi205num;
+@property (nonatomic, copy) NSString *mi206num;
+@property (nonatomic, copy) NSString *mi207num;
+@property (nonatomic, copy) NSString *mi208num;
+@property (nonatomic, copy) NSString *mi209num;
+@property (nonatomic, copy) NSString *mi20num;
+@property (nonatomic, copy) NSString *mi21;
+@property (nonatomic, copy) NSString *mi21num;
+@property (nonatomic, copy) NSString *mi2num;
+@property (nonatomic, copy) NSString *mi3num;
+@property (nonatomic, copy) NSString *mi4num;
+@property (nonatomic, copy) NSString *mi5num;
+@property (nonatomic, copy) NSString *mi6num;
+@property (nonatomic, copy) NSString *mi7num;
+@property (nonatomic, copy) NSString *mi8num;
+@property (nonatomic, copy) NSString *mi9num;
+@property (nonatomic, copy) NSString *orgid;
+@property (nonatomic, copy) NSString *orgname;
+@property (nonatomic, copy) NSString *remark;
+@property (nonatomic, copy) NSString *work;
+
 @end
 
 NS_ASSUME_NONNULL_END
