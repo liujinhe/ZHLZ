c247c665e68f017e82e5d1f497348b2104b26450
lijinhui <854816128@qq.com>
2019-12-02 12:05:45 +0800

    李锦辉提交==

---
+++ ZHLZ/CoreBusiness/Other/ViewModel/ZHLZUploadVM.h
@@ -0,0 +1,17 @@
+//
+//  ZHLZUploadVM.h
+//  ZHLZ
+//
+//  Created by apple on 2019/12/2.
+//  Copyright © 2019 liujinhe. All rights reserved.
+//
+
+#import "ZHLZBaseVM.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZHLZUploadVM : ZHLZBaseVM
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZHLZ/CoreBusiness/Other/ViewModel/ZHLZUploadVM.m
@@ -0,0 +1,13 @@
+//
+//  ZHLZUploadVM.m
+//  ZHLZ
+//
+//  Created by apple on 2019/12/2.
+//  Copyright © 2019 liujinhe. All rights reserved.
+//
+
+#import "ZHLZUploadVM.h"
+
+@implementation ZHLZUploadVM
+
+@end
