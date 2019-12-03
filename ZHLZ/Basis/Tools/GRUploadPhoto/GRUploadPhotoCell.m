4a7a14e97391a99c3bc415cccc9d2109fc541615
lijinhui <854816128@qq.com>
2019-12-03 16:09:08 +0800

    李锦辉提交==

--- ZHLZ/Basis/Tools/GRUploadPhoto/GRUploadPhotoCell.m
+++ ZHLZ/Basis/Tools/GRUploadPhoto/GRUploadPhotoCell.m
@@ -19,7 +19,7 @@
         self.backgroundColor = UIColor.whiteColor;
         
         _imageView = [[UIImageView alloc] init];
-        _imageView.contentMode = UIViewContentModeScaleAspectFill;
+        _imageView.contentMode = UIViewContentModeScaleAspectFit;
         [self addSubview:_imageView];
         
         self.clipsToBounds = YES;
